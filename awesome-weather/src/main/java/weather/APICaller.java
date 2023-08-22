5c5c5df112a8aa1b8ff7ce3fa418f8e16c4009c2
waifuisalie <stefanme48@gmail.com>
2023-08-22 16:50:19 -0300

    Better names
    also there is something wrong with the URLs

--- awesome-weather/src/main/java/weather/APICaller.java
+++ awesome-weather/src/main/java/weather/APICaller.java
@@ -12,20 +12,25 @@ import java.util.Scanner;
 
 public class APICaller {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("What city do you want to check? ");
-        String user_city = scanner.nextLine();
-
-        location_api_parser(user_city);
-
-        System.out.print("Enter the index you wanna choose: ");
-        int user_choice = scanner.nextInt();
-
-        temp_api_parser(user_choice, user_city);
-
-        scanner.close();
+        APICaller apiCaller = new APICaller();
+        apiCaller.run();
     }
     
+    private static String locationApiUrl(String user_city) {
+        String apiKey = System.getenv("MY_API_KEY");
+        String apiUrlFormat = "http://dataservice.accuweather.com/locations/v1/cities/search?apikey=%s&q=%s";
+        String locationApiUrl = String.format(apiUrlFormat, apiKey, user_city);
+        return locationApiUrl;
+    }
+    
+    private static String keyApiUrl(int user_choice) {
+        String apiKey = System.getenv("MY_API_KEY");
+        String apiUrlFormatTemp = "http://dataservice.accuweather.com/currentconditions/v1/%s?apikey=%s";
+        String chosenKey = fetchChosenKey(user_choice, apiUrlFormatTemp);
+        String apiUrlTemp = String.format(apiUrlFormatTemp, chosenKey, apiKey);
+        return apiUrlTemp;
+    }
+
     private static JSONArray generateJsonArrayForApiCall(String apiUrl) {
         HttpClient httpClient = HttpClients.createDefault();
         try {
@@ -40,16 +45,10 @@ public class APICaller {
         }
     }
 
-    private static String location_api_url(String user_city) {
-        String apiKey = System.getenv("MY_API_KEY");
-        String apiUrlFormat = "http://dataservice.accuweather.com/locations/v1/cities/search?apikey=%s&q=%s";
-        String locationApiUrl = String.format(apiUrlFormat, apiKey, user_city);
-        return locationApiUrl;
-    }
-
-    private static void location_api_parser(String user_city) {
-        String apiUrl = location_api_url(user_city);
+    private static void fetchLocationData(String user_city) {
+        String apiUrl = locationApiUrl(user_city);
         JSONArray jsonArray = generateJsonArrayForApiCall(apiUrl);
+        
 
         if (jsonArray != null) {
             System.out.println("Available cities:");
@@ -57,26 +56,18 @@ public class APICaller {
                 JSONObject jsonObject = jsonArray.getJSONObject(i);
                 JSONObject administrativeArea = jsonObject.getJSONObject("AdministrativeArea");
                 JSONObject country = jsonObject.getJSONObject("Country");
-                String state_name = administrativeArea.getString("LocalizedName");
-                String country_name = country.getString("LocalizedName");
+                String stateName = administrativeArea.getString("LocalizedName");
+                String countryName = country.getString("LocalizedName");
                 
-                System.out.println("(" + i + ") " + user_city + ", " + state_name + " from " + country_name);
+                System.out.println("(" + i + ") " + user_city + ", " + stateName + " from " + countryName);
             }
         } else {
             System.out.print("Error fetching data for locations.");
         }        
     }
 
-    private static String key_api_url(int user_choice) {
-        String apiKey = System.getenv("MY_API_KEY");
-        String apiUrlFormatTemp = "http://dataservice.accuweather.com/currentconditions/v1/%s?apikey=%s";
-        String chosenKey = chosen_key_getter(user_choice, apiUrlFormatTemp);
-        String apiUrlTemp = String.format(apiUrlFormatTemp, chosenKey, apiKey);
-        return apiUrlTemp;
-    }
-
-    private static String chosen_key_getter(int user_choice, String user_city) {
-        String apiUrl = location_api_url(user_city);
+    private static String fetchChosenKey(int user_choice, String user_city) {
+        String apiUrl = locationApiUrl(user_city);
         JSONArray jsonArray = generateJsonArrayForApiCall(apiUrl);
         
         if (user_choice >= 0 && user_choice < jsonArray.length()) {
@@ -88,11 +79,11 @@ public class APICaller {
                 return null;
         }
     }
-    
-    private static void temp_api_parser(int user_choice, String user_city) {
-        String chosen_key_string = chosen_key_getter(user_choice, user_city);
+
+    private static void fetchTemperatureData(int user_choice, String user_city) {
+        String chosen_key_string = fetchChosenKey(user_choice, user_city);
         int chosen_key_int = Integer.parseInt(chosen_key_string);
-        String apiUrlTemp = key_api_url(chosen_key_int);
+        String apiUrlTemp = keyApiUrl(chosen_key_int);
         JSONArray jsonArray2 = generateJsonArrayForApiCall(apiUrlTemp);
 
         if (jsonArray2 != null) {
@@ -109,5 +100,19 @@ public class APICaller {
         } else {
             System.out.print("Error fetching data for temperatures.");
         }
+    }
+    public void run() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("What city do you want to check? ");
+        String user_city = scanner.nextLine();
+
+        fetchLocationData(user_city);
+
+        System.out.print("Enter the index you wanna choose: ");
+        int user_choice = scanner.nextInt();
+
+        fetchTemperatureData(user_choice, user_city);
+
+        scanner.close();
     }
 }
